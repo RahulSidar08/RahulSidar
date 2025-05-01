e3a072691dd9d7baeb92473e5c9238ccb825f8e0
RahulSidar08 <rahulsidar2056@gmail.com>
2025-05-01 13:05:47 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Navbar } from './components/Navbar'
 import { About } from './components/About'
 import Skills from './components/Skills'
 import Contact from './components/Contact'
-import { HeroSection } from './components/Herosection'
+import { HeroSection } from './components/HeroSection'
 import { Project } from './components/Project'
 
 function App() {
